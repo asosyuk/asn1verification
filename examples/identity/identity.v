7499fe74726f996510b58ecd7e7cc9f37e2ae294
Ilia Zaichuk <izaichuk@digamma.ai>
2019-07-15 17:57:46 +0300

    Add implication-based identity proof

--- examples/identity/identity.v
+++ examples/identity/identity.v
@@ -16,50 +16,34 @@ Definition f_identity := {|
 (Sreturn (Some (Etempvar _n tint)))
 |}.
 
-Definition identity_statement := f_identity.(fn_body).
+Definition identity_statement := fn_body f_identity.
 
 Definition identity_fspec (n : Z) := n.
 
-Section ID0.
-  
-  Let env0 : temp_env := Maps.PTree.set _n (Vint (Int.repr 0)) (Maps.PTree.empty val).
-
-  Lemma f_identity0 (any_genv : genv) (any_env : env) (any_mem : Memory.Mem.mem) :
-    exists (res_tr : trace) (res_te : temp_env),
-      exec_stmt
-      any_genv
-      any_env
-      env0
-      any_mem
-      identity_statement
-      res_tr
-      res_te
-      any_mem
-      (Out_return (Some ((Vint (Int.repr 0), tint)))).
-  Proof.
-    repeat eexists.
-    repeat constructor.
-  Qed.
-
-End ID0.
-
-Theorem f_identity_correct (any_genv : genv) (any_env : env) (any_mem : Memory.Mem.mem) :
-  forall (tenv : temp_env) (n : Z),
-    tenv!_n = Some (Vint (Int.repr n)) ->
-    exists (res_tr : trace) (res_te : temp_env),
-      exec_stmt
-      any_genv
-      any_env
-      tenv
-      any_mem
-      identity_statement
-      res_tr
-      res_te
-      any_mem
+Theorem f_identity_correct_ex (ge : genv) (e : env) (m : Memory.Mem.mem)
+        (te : temp_env) (n : Z) :
+    te!_n = Some (Vint (Int.repr n)) ->
+    exists (t : trace) (te' : temp_env),
+      exec_stmt ge e te m identity_statement t te' m
       (Out_return (Some ((Vint (Int.repr (identity_fspec n)), tint)))).
 Proof.
   intros.
   repeat eexists.
+  repeat constructor.
+  assumption.
+Qed.
+
+(*
+ * this statement is stronger, proving that
+ * memory does not change and trace is nil
+ *)
+Theorem f_identity_correct_impl (ge : genv) (e : env) (m : Memory.Mem.mem)
+        (te : temp_env) (n : Z) :
+    te!_n = Some (Vint (Int.repr n)) ->
+      exec_stmt ge e te m identity_statement nil te m
+      (Out_return (Some ((Vint (Int.repr (identity_fspec n)), tint)))).
+Proof.
+  intros.
   repeat constructor.
   assumption.
 Qed.
