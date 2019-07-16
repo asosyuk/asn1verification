718535236609f0fd872dca0657743c9b53120ecf
Ilia Zaichuk <izaichuk@digamma.ai>
2019-07-16 21:43:03 +0300

    Prove pow2 correctness

--- examples/pow2/pow2.v
+++ examples/pow2/pow2.v
@@ -1,6 +1,8 @@
 From Coq Require Import String List ZArith.
 From compcert Require Import Coqlib Integers Floats AST Ctypes Cop Clight Clightdefs.
 From compcert Require Import Maps Values ClightBigstep Events.
+  
+Require Import Lia.
 
 Local Open Scope Z_scope.
 
@@ -28,116 +30,173 @@ Definition f_pow2 := {|
     (Sreturn (Some (Etempvar _res tuint)))))
 |}.
 
-Definition f_pow2_iteration :=
-  (Ssequence
-    (Sset _res
-      (Ebinop Oadd (Etempvar _res tuint) (Etempvar _res tuint) tuint))
-    (Sset _n
-      (Ebinop Osub (Etempvar _n tuint) (Econst_int (Int.repr 1) tint)
-        tuint))).
-
 Definition f_pow2_loop :=
   (Swhile
     (Etempvar _n tuint)
-    f_pow2_iteration).
+    (Ssequence
+      (Sset _res
+        (Ebinop Oadd (Etempvar _res tuint) (Etempvar _res tuint) tuint))
+      (Sset _n
+        (Ebinop Osub (Etempvar _n tuint) (Econst_int (Int.repr 1) tint)
+          tuint)))).
+
+Definition Vnat (n : nat) := Vint (Int.repr (Z.of_nat n)).
+Definition Mset' := Maps.PTree.set.
+Definition Mset := Mset' val.
+
+Ltac gso := rewrite PTree.gso by discriminate.
+Ltac gss := apply PTree.gss.
+
+Lemma succ_not_zero_int (n : nat) :
+  Z.of_nat (S n) < Int.modulus ->
+  Int.repr (Z.of_nat (S n)) <> Int.repr (Z.of_nat O).
+Proof.
+  intros; intro H1.
+  apply f_equal with (f := Int.unsigned) in H1.
+  repeat rewrite Int.unsigned_repr_eq in H1.
+  rewrite Zmod_small, Zmod_0_l in H1
+    by (auto with zarith).
+  inversion H1.
+Qed.
 
 Definition pow2_fspec (n : nat) := Nat.pow 2 n.
 
-Definition Vint_of_nat (n : nat) := Vint (Int.repr (Z_of_nat n)).
+Lemma pow2_fspec_S_geq_2 (n : nat) :
+  (2 <= pow2_fspec (S n))%nat.
+Proof.
+  unfold pow2_fspec.
+  assert (2 = 2^1)%nat by trivial; rewrite H at 1; clear H.
+  apply Nat.pow_le_mono_r; lia.
+Qed.
 
-Lemma pow2_iteration_correct : forall ge e m res, forall n le,
-      0 <= Z.of_nat res <= Int.max_unsigned ->
+Lemma pow2_fspec_S (n : nat) :
+  (pow2_fspec (S n) = 2 * pow2_fspec n)%nat.
+Proof.
+  unfold pow2_fspec.
+  simpl.
+  lia.
+Qed.
 
-      le!_n = Some (Vint_of_nat n) ->
-      le!_res = Some (Vint_of_nat res) ->
-      
-      exists t le',
-        exec_stmt ge e le m f_pow2_iteration t le' m Out_normal
-        /\ (le'!_res) = Some (Vint_of_nat (res + res)).
-Admitted.
-
-
-Lemma pow2_loop_correct : forall ge e m, forall le, forall n res,
+Lemma f_pow2_loop_correct : forall ge e m, forall n res le,
       Z.of_nat n < Int.modulus ->
       (* 1 <= *) Z.of_nat res < Int.modulus ->
       Z.of_nat (res * (pow2_fspec n)) < Int.modulus ->
       
-      le!_n = Some (Vint_of_nat n) ->
-      le!_res = Some (Vint_of_nat res) ->
+      le!_n = Some (Vnat n) ->
+      le!_res = Some (Vnat res) ->
       
       exists t le',
         exec_stmt ge e le m f_pow2_loop t le' m Out_normal
-        /\ (le'!_res) = Some (Vint_of_nat (res * (pow2_fspec n))).
+        /\ (le'!_res) = Some (Vnat (res * (pow2_fspec n))).
 Proof.
   induction n; intros.
   - repeat eexists.
     + eapply exec_Sloop_stop1.
-      * eapply exec_Sseq_2.
-        repeat econstructor.
-        eassumption.
-        econstructor.
-        econstructor.
-        discriminate.
-      * constructor.
-    + replace (res * pow2_fspec 0)%nat with res.
+      eapply exec_Sseq_2.
+      repeat econstructor.
+      all: repeat econstructor.
+      all: try eassumption; try discriminate.
+      econstructor.
+      econstructor.
+    + replace (res * pow2_fspec 0)%nat with res
+        by (rewrite Nat.mul_1_r; reflexivity).
       assumption.
-      rewrite Nat.mul_1_r; reflexivity.
-  - repeat eexists.
-    + unfold f_pow2_loop.
-      unfold Swhile.
-      eapply exec_Sloop_loop.
-      * repeat econstructor.
-        all: try eassumption.
-        all: try econstructor.
-        -- replace (negb (Int.eq (Int.repr (Z.of_nat (S n))) Int.zero)) with true by admit.
-           econstructor.
-        -- rewrite PTree.gso by discriminate.
-           eassumption.
-        -- econstructor.
-      * constructor.
-      * econstructor.
-      * (* time to apply induction hypothesis here? *)
-        admit.
-    +  (* and here? *)
-      admit.
-Abort.
+  - assert
+      (
+        let step_le := (Mset _n (Vnat n)
+                            (Mset _res (Vnat (res * 2))
+                                  le)) in
+        exists (t : trace) (le' : temp_env),
+          exec_stmt ge e step_le m f_pow2_loop t le' m Out_normal
+          /\
+          le' ! _res =  Some (Vnat ((res * 2) * (pow2_fspec n) ))
+      ).
+    {
+      intro; subst step_le.
+      apply IHn.
+      - lia.
+      - pose proof pow2_fspec_S_geq_2 n.
+        nia.
+      - rewrite <-Nat.mul_assoc, <-pow2_fspec_S.
+        assumption.
+      - apply PTree.gss.
+      - gso; gss.
+    }
+    destruct H4. destruct H4. destruct H4.
+    repeat eexists;
+     [| rewrite pow2_fspec_S, Nat.mul_assoc; eassumption].
+    eapply exec_Sloop_loop.
+    eapply exec_Sseq_1.
+    2: eapply exec_Sseq_1.
+    econstructor.
+    econstructor.
+    eassumption.
+    econstructor.
+    rewrite Int.eq_false
+       by (apply succ_not_zero_int; assumption); simpl.
+    econstructor.
+    repeat econstructor.
+    eassumption.
+    eassumption.
+    repeat econstructor.
+    repeat econstructor.
+    gso; eassumption.
+    repeat econstructor.
+    econstructor.
+    econstructor.
+    replace 
+      (PTree.set _n (Vint (Int.sub (Int.repr (Z.of_nat (S n)))
+                                       (Int.repr 1)))
+        (PTree.set _res (Vint (Int.add (Int.repr (Z.of_nat res))
+                                          (Int.repr (Z.of_nat res))))
+          le))
+      with
+        (Mset _n (Vnat n)
+                  (Mset _res (Vnat (res * 2))
+                        le)).
+    eassumption.
+    unfold Mset, Mset', Vnat.
+    repeat f_equal.
+    1: unfold Int.sub.
+    2: unfold Int.add.
+    all: repeat rewrite Int.unsigned_repr_eq;
+      f_equal; repeat rewrite Zmod_small; nia.
+Qed.
 
-Lemma pow2_iteration_correct' : forall ge e m res n le t le',
-      0 <= Z.of_nat res <= Int.max_unsigned ->
-
-      le!_n = Some (Vint_of_nat n) ->
-      le!_res = Some (Vint_of_nat res) ->
-      
-      exec_stmt ge e le m f_pow2_iteration t le' m Out_normal ->
-      (le'!_res) = Some (Vint_of_nat (res + res)).
+Theorem f_pow2_correct : forall ge e m n le,
+    Z.of_nat n < Int.modulus ->
+    Z.of_nat (pow2_fspec n) < Int.modulus ->
+    le!_n = Some (Vnat n) ->
+    exists t le' out,
+      exec_stmt ge e le m (fn_body f_pow2) t le' m out
+      /\
+      (le'!_res) = Some (Vnat (pow2_fspec n)).
 Proof.
   intros.
-  inversion H2; subst; [| contradict H13; reflexivity].
-  inversion H8; subst.
-  inversion H13; subst.
-  rewrite PTree.gso by discriminate.
-  rewrite PTree.gss.
-  f_equal.
-  clear H2 H8 H11 H13 H14.
-  inversion H12; subst.
-  - simpl in H9.
-    admit.
-  - admit.
-Admitted.
-
-Lemma pow2_loop_correct_alt : forall ge e m le n res t le',
-      Z.of_nat n < Int.modulus ->
-      (* 1 <= *) Z.of_nat res < Int.modulus ->
-      Z.of_nat (res * (pow2_fspec n)) < Int.modulus ->
-      
-      le!_n = Some (Vint_of_nat n) ->
-      le!_res = Some (Vint_of_nat res) ->
-      
-      exec_stmt ge e le m f_pow2_loop t le' m Out_normal ->
-      (le'!_res) = Some (Vint_of_nat (res * (pow2_fspec n))).
-Proof.
-  induction n.
-  intros.
-  - inversion H4; subst.
-    inversion H10; subst.
-Admitted.
+  assert
+    (
+      exists t le',
+        exec_stmt ge e (Mset _res (Vnat 1) le) m f_pow2_loop t le' m Out_normal
+        /\
+        le' ! _res = Some (Vnat (1 * (pow2_fspec n)))
+    ).
+  {
+    eapply f_pow2_loop_correct.
+    + apply H.
+    + split.
+    + nia.
+    + rewrite PTree.gso. apply H1. cbv. congruence.
+    + apply PTree.gss.
+  }
+  destruct H2. destruct H2.  destruct H2.
+  eexists. eexists.
+  exists (Out_return (Some (Vnat (pow2_fspec n), tuint))).
+  split.
+  + eapply exec_Sseq_1.
+    econstructor. econstructor.
+    eapply exec_Sseq_1.
+    apply H2.
+    repeat econstructor; rewrite Nat.mul_1_l in H3; assumption.
+  + rewrite Nat.mul_1_l in H3.
+    assumption.
+Qed.
