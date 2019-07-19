145a8353c0192ce62d071ad169e08fd04e43fa65
Ilia Zaichuk <izaichuk@digamma.ai>
2019-07-19 17:45:24 +0300

    Remove junk files

--- .coqdeps.d
+++
@@ -1,2 +0,0 @@
-asn_strtoimax_lim_spec.vo asn_strtoimax_lim_spec.glob asn_strtoimax_lim_spec.v.beautified: asn_strtoimax_lim_spec.v ./IntNotations.vo ./asn_strtoimax_lim.vo
-asn_strtoimax_lim_spec.vio: asn_strtoimax_lim_spec.v ./IntNotations.vio ./asn_strtoimax_lim.vio
