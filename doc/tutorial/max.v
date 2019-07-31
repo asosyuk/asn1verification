7e05ac2417cde3fc909a73cd1fbd54d8e4da8b79
Ilia Zaichuk <izaichuk@digamma.ai>
2019-07-31 19:20:38 +0300

    Refactor [max] example, add comments

--- doc/tutorial/max.v
+++ doc/tutorial/max.v
@@ -6,18 +6,25 @@ Require Import Clight.max.
 
 Open Scope Z_scope.
 
+Definition max_fspec := Z.max.
+
 Theorem f_max_correct (ge : genv) (e : env) (m : Memory.Mem.mem) :
   forall (ste : temp_env) (n1 n2 : Z),
+    (* starting environment *)
     ste ! _n1 = Some (Vint (Int.repr n1)) ->
     ste ! _n2 = Some (Vint (Int.repr n2)) ->
+
+    (* bounds on coq representation *)
     Int.min_signed <= n1 <= Int.max_signed ->
     Int.min_signed <= n2 <= Int.max_signed ->
+
+    (* correct return *)
     exists (t : trace) (rte : temp_env),
       exec_stmt ge e ste m f_max.(fn_body) t rte m
-        (Out_return (Some ((Vint (Int.repr (Z.max n1 n2)), tint)))).
+        (Out_return (Some ((Vint (Int.repr (max_fspec n1 n2)), tint)))).
 Proof.
-  (** the two branches of this proof are exactly the same,
-      but have been left as is for simplicity *)
+  (* the two branches of this proof are exactly the same,
+     but have been left as is for simplicity *)
   intros.
   (* introduce existential variables *)
   destruct (Z_le_dec n1 n2).
