3ef8d83e5f64d23982b7ad980c2c03fdf9614d5f
p3rsik <yroslav541@gmail.com>
2019-12-11 19:09:31 +0200

    Initial commit for VST#master version

---
+++ doc/tutorial/C/intmax_test.c
@@ -0,0 +1,6 @@
+#include "inttypes.h"
+
+void long_test() {
+  intmax_t a = 1, b = 1;
+  a += b;
+}
--- src/Core/Tactics.v
+++ src/Core/Tactics.v
@@ -39,27 +39,6 @@ Ltac bool_rewrite :=
   | [ H : ?X = false |- context[?X] ] => rewrite H
   end.
 
-Ltac choose_seq s1 :=
-  match goal with
-  | [ |- exec_stmt _ _ _ _ (Ssequence s1 _)
-                  _ _ _ _ ] => eapply exec_Sseq_2 
-  | [ |- exec_stmt _ _ _ _ (Ssequence _ _ )
-                  _ _ _ _ ] => eapply exec_Sseq_1
-  end.
-
-Ltac exec_Axiom :=
-  match goal with
-  | [ |- context [exec_stmt _ _ _ _ Sskip _ _ _ Out_normal]] => econstructor
-  | [ |- context [exec_stmt _ _ _ _ Scontinue _ _ _ Out_continue]] => econstructor
-  | [ |- context [exec_stmt _ _ _ _ Sbreak _ _ _ Out_break]] => econstructor
-  end.
-
-Ltac exec_loop_continue := 
-  repeat match goal with
-         | [ |- exec_stmt _ _ _ (Sloop _) _ _ _ _ ] => idtac
-         | _ => econstructor ; exec_loop_continue
-         end.
-
 Ltac destruct_orb_hyp :=
  match goal with
  | [H : orb _ _ = true |- _] => apply orb_prop in H; destruct H
@@ -71,42 +50,3 @@ Ltac destruct_andb_hyp :=
  | [H : andb _ _ = true |- _] => apply andb_prop in H; destruct H
  | [H : andb _ _ = false |- _] => apply andb_false_elim in H; destruct H
  end.
-
-Ltac gso_simpl := rewrite PTree.gso by discriminate.
-Ltac gss_simpl := rewrite PTree.gss.
-
-Ltac env_assumption := try (eassumption || gso_simpl || gss_simpl).
-
-Ltac exec_until_seq := 
-  repeat  match goal with
-          | [ |- exec_stmt _ _ _ _ (Ssequence _ _) _ _ _ _ ] => idtac
-          | _ => econstructor ; exec_until_seq
-          end.
-
-Ltac forward := repeat (econstructor || env_assumption).
-
-Ltac break_ife_true := match goal with
-                       | [ |- exec_stmt _ _ _ _ (if ?X then _ else _ ) _ _ _ _ ] => replace X with true
-                       end.
-
-Ltac break_ife_false := match goal with
-                        | [ |- exec_stmt _ _ _ _ (if ?X then _ else _ ) _ _ _ _ ] => replace X with false
-                        end.
-
-
-Theorem set_env_eq_ptree_set : forall (A : Type) (te : PTree.t A) a b, 
-    (in te set [ a <~ b ]) = (PTree.set a b te).
-Proof.
- intros. 
- unfold s.
- simpl.
- reflexivity.
-Qed.
-
-Ltac gso := rewrite set_env_eq_ptree_set; rewrite PTree.gso.
-Ltac gss := rewrite set_env_eq_ptree_set; rewrite PTree.gss.
-
-Ltac seq1 := eapply exec_Sseq_1.
-Ltac seq2 := eapply exec_Sseq_2.
-Ltac sset := eapply exec_Sset.
-Ltac loop := eapply exec_Sloop_loop.
