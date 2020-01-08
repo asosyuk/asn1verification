872bd2bfb844b380b87bdc41e07b363e3c191a8a
Nika <nikaponens@gmail.com>
2020-01-08 19:18:12 +0200

    seq type

--- model/C_Model.v
+++ model/C_Model.v
@@ -4,26 +4,49 @@ Open Scope Z.
 
 Parameter binary : Type.
 Parameter asn_value : Type.
+Parameter context : Type.
+Definition tag := Z.
+
+Record seq_specifics := { ctx : context ;
+                 tag_to_el : tag -> nat  
+               }.
 
 Inductive TYPE_descriptor :=
   { name : string ;
-    tags : list Z ;
+    tags : list tag ;
     elements : list TYPE_member ;
-    encode : asn_value -> binary ;
-    decode : binary -> asn_value 
+    decode : binary -> asn_value ;
+    seq_specs : list seq_specifics
   }
     with TYPE_member :=
-           { mem_id : string ;
-             outmost_tag : Z ;
+           { mem_tag : Z*Z ; (* tag and flag: implicit *)
              type : TYPE_descriptor ;
-             offset : Z
            }.
 
-Parameter bool_encode : asn_value -> binary.
+(* Primitive types *)
 Parameter bool_decode : binary -> asn_value.
+Definition BOOLEAN := {| name := "BOOLEAN";
+                         tags := [1];
+                         elements := [];
+                         decode := bool_decode;
+                         seq_specs := []
+                      |}.
+
+Parameter integer_decode : binary -> asn_value.
+Definition INTEGER := {| name := "INTEGER";
+                         tags := [2];
+                         elements := [];
+                         decode := integer_decode;
+                         seq_specs := []
+                      |}.
+
+(* Constructed types *)
+Parameter seq_decode : binary -> TYPE_descriptor -> asn_value.
 
 Definition BOOLEAN := {| name := "BOOLEAN";
                          tags := [1];
                          elements := [];
-                         encode := bool_encode;
-                         decode := bool_decode |}.
+                         decode := bool_decode;
+                         seq_specs := []
+                      |}.
+
