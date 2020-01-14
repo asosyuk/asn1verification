d33ddcbd313535995d7c23f68407b7a2bb633416
Nika <npona@digamma.ai>
2020-01-14 21:24:24 +0200

    ber_input and primitive decoders variant

--- model/C_Model.v
+++ model/C_Model.v
@@ -2,6 +2,85 @@ Require Import Nat List ZArith Decimal DecimalNat List Lia String.
 Import ListNotations.
 Open Scope Z.
 
+Section Tables.
+
+Definition bit := bool.
+
+Inductive ber_input :=
+  | prim_input : nat -> nat -> list bit -> ber_input
+  | const_input : nat -> nat -> list ber_input -> ber_input.
+  
+Check (const_input 16 3 [prim_input 1 1 [true]]).
+
+Inductive asn_value := OK | ERROR.
+
+(* read until the end of the data, otherwise error *)
+Fixpoint primitive_decode (len : nat) (input : list bit) :=
+  match len, input with
+  | O, _ => OK
+  | S n, [] => ERROR
+  | S n, hd :: tl => primitive_decode n tl
+  end.
+
+Fixpoint decode (input : ber_input) :=
+  match input with
+  | prim_input tag len ls => primitive_decode len ls
+  | const_input tag len ls => match ls with 
+                             | [] => OK
+                             | hd :: tl => decode hd 
+                             end
+  end.
+
+Parameter context : Type.
+
+Definition tag := Z.
+
+Record seq_specifics := {
+                         ctx : context ; 
+                         tag_to_el : tag -> nat  
+                       }.
+
+Inductive TYPE_descriptor :=
+  { name : string ;
+    tags : list Z ;
+    elements : list TYPE_member ;
+    decode : list bit -> nat -> asn_value ;
+    seq_specs : list seq_specifics
+  }
+    with TYPE_member :=
+           { mem_tag : Z*Z ; (* tag and flag: implicit *)
+             type : TYPE_descriptor ;
+           }.
+
+(* Primitive types *)
+
+                               
+Definition BOOLEAN := {| name := "BOOLEAN";
+                         tags := [1];
+                         elements := [];
+                         decode := primitive_decode;
+                         seq_specs := []
+                      |}.
+
+Definition INTEGER := {| name := "INTEGER";
+                         tags := [2];
+                         elements := [];
+                         decode := primitive_decode;
+                         seq_specs := []
+                      |}.
+
+(* Constructed types *)
+  
+
+Fixpoint seq_decode (input : list bit) (DEF : TYPE_descriptor) (len : nat) : asn_value 
+  :=  match elements DEF with
+      | [] => OK
+      | hd :: tl => (decode (type hd)) input len 
+      end. 
+
+End Tables.
+
+(*
 Set Implicit Arguments.
 
 Record fsa (S A : Type) := FSA {
@@ -41,60 +120,4 @@ Section BOOL_automaton.
   Definition BOOL_fsa := {| initial := INIT ;
                             next := BOOL_next |}.
 End BOOL_automaton.
-
-Section Tables.
-
-Parameter asn_value : Type.
-Parameter context : Type.
-
-Definition tag := Z.
-
-Record seq_specifics := {
-                         ctx : context ; 
-                         tag_to_el : tag -> nat  
-                       }.
-
-Inductive TYPE_descriptor :=
-  { name : string ;
-    tags : list tag ;
-    elements : list TYPE_member ;
-    decode : list Z -> asn_value -> Prop ;
-    seq_specs : list seq_specifics
-  }
-    with TYPE_member :=
-           { mem_tag : Z*Z ; (* tag and flag: implicit *)
-             type : TYPE_descriptor ;
-           }.
-
-(* Primitive types *)
-
-Parameter asn_true : asn_value.
-Parameter asn_false : asn_value.
-
-Inductive bool_decode : list Z -> asn_value -> Prop :=
-  | dec_true ls : fsa_run BOOL_fsa ls (TRUE 0) ->
-                      bool_decode ls asn_true
-  | dec_false ls : fsa_run BOOL_fsa ls (FALSE 0) ->
-                      bool_decode ls asn_false.
-  
-                                
-Definition BOOLEAN := {| name := "BOOLEAN";
-                         tags := [1];
-                         elements := [];
-                         decode := bool_decode;
-                         seq_specs := []
-                      |}.
-
-Parameter integer_decode : list Z -> asn_value -> Prop.
-Definition INTEGER := {| name := "INTEGER";
-                         tags := [2];
-                         elements := [];
-                         decode := integer_decode;
-                         seq_specs := []
-                      |}.
-
-(* Constructed types *)
-
-Parameter seq_decode : list Z -> TYPE_descriptor -> asn_value.
-
-End Tables.
+*)
