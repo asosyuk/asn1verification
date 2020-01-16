628f00e9c928cab7430e53d886fd8d80f70cf3aa
Nika <npona@digamma.ai>
2020-01-16 18:53:14 +0200

    replace bit_word with Integer.byte; draft of automaton on bytes and DEF table

--- model/C_Model.v
+++ model/C_Model.v
@@ -1,54 +1,40 @@
 Require Import Nat List ZArith Decimal DecimalNat List Lia.
 Import ListNotations.
 
-Definition bit := bool.
+From compcert Require Import Integers.
 
-Inductive bit_word : nat -> Type  := 
-  | empty : bit_word O
-  | w n : bool -> bit_word n -> bit_word (S n).
+Inductive asn_value := OK | ERROR.
 
-Definition octet := bit_word 8.
- 
-Inductive asn_value := OK | MORE | ERROR.
-
-Parameter bit_word_to_nat : forall n, bit_word n -> nat.
+Parameter byte_to_nat : byte -> nat.
 
 Section Tables.
 
 Inductive TYPE_descriptor :=
-  { id : nat ;
-    primitive : bool ;
-    tags : list nat ;
+  { primitive : bool ;
+    tags : list Z ;
     elements : list TYPE_descriptor ;
-    decoder : nat -> list bit -> asn_value ;
+    decoder : list byte -> asn_value ;
   }.
 
 (* Primitive types *)
 
-(* read until the end of the data, otherwise MORE error *)
-Definition primitive_decode (len : nat) (input : list bit) :=
-  if len <=? length input then OK else MORE.
+(* read until the end of the data *)
+Definition primitive_decode (input : list byte) := OK.
  
+Open Scope Z.
                
-Definition BOOLEAN := {| id := 1;
-                         primitive := true;
+Definition BOOLEAN := {| primitive := true;
                          tags := [1];
                          elements := [];
                          decoder := primitive_decode;
                       |}.
 
-Definition INTEGER := {| id := 2;
-                         primitive := true;
+Definition INTEGER := {| primitive := true;
                          tags := [2];
                          elements := [];
                          decoder := primitive_decode;
+
                       |}.
-
-Parameter bits_to_nat : list bit -> nat.
-
-(* Check if tag is among allowed tags for this structure *)
-Definition check_tag (input : list bit) (DEF : TYPE_descriptor) :=
-  existsb (fun z => z =? (bits_to_nat input)) (tags DEF).
 
 End Tables.
 
@@ -56,38 +42,48 @@ Section PRIM_automaton.
 
   Set Implicit Arguments.
 
+  Record fsa (S A : Type) := FSA {
+                                 initial : S;
+                                 next : S -> A -> S -> Prop
+                               }.
+
+  Inductive fsa_run {S A : Type } (m : fsa S A) : list A -> S -> Prop :=
+  | step : forall a s, next m (initial m) a s -> fsa_run m [a] s
+  | trans : forall s1 a s2 l rs, next m s1 a s2 -> fsa_run m l rs -> fsa_run m (a::l) rs.
+
   Inductive PRIM_state :=
-  | INIT
+  | INIT (X : TYPE_descriptor)
   | TAG
-  | LEN (n : nat) (* store length *)
-  | DEC (l : nat) (v : bit_word l). (* store value *)
+  | DEC  (l : nat) (v : list byte)
+  | RES (r : asn_value).
   
   Reserved Notation "st1 -[ c ]-> st2" (at level 50).
   
-  Inductive PRIM_next : PRIM_state -> forall n, bit_word n -> PRIM_state -> Prop :=
-  | read_tag : forall t : bit_word 8,
-      INIT -[ t ]-> TAG
-  | read_length : forall l : bit_word 8,
-      TAG -[ l ]-> LEN (bit_word_to_nat 8 l)
-  | read_value : forall l (v : bit_word l), LEN l -[ v ]-> DEC v                    
+  Inductive PRIM_next : PRIM_state -> byte -> PRIM_state -> Prop :=
+  | check_tag t X : 
+      In (Byte.unsigned t) (tags X) ->
+      INIT X -[ t ]-> TAG 
+  | read_prim_length l X : 
+      primitive X = true ->
+      TAG  -[ l ]-> DEC (byte_to_nat l) []
+  | read_value l vl v : 
+      1 < l ->
+      DEC l vl -[ v ]-> DEC (l-1)%nat (v::vl) 
+  | call_prim_decoder vl v X: 
+      DEC 1 vl -[ v ]-> RES (decoder X (v::vl))   
+  | read_constr_length l X Y n: 
+      n < length (elements X) ->
+      nth n (elements X) X = Y ->
+      primitive X = false ->
+      TAG -[ l ]-> INIT Y 
 
   where "st1 -[ c ]-> st2" := (PRIM_next st1 c st2).
 
-  Parameter bit_concat : forall {n} {m}, bit_word n -> bit_word m -> bit_word (n + m).
+  Definition PRIM_fsa X := {| initial := INIT X ; 
+                              next := PRIM_next |}.
 
-  Notation "a ∘ b" := (bit_concat a b) (at level 25).
-
-  Reserved Notation "st1 -[ c ]*-> st2" (at level 50).
-
-  Inductive PRIM_star : PRIM_state -> forall n, bit_word n -> PRIM_state -> Prop :=
-  | base :  forall t : bit_word 8, INIT -[ t ]*-> TAG
-  | step :  forall s1 s2 s3
-              n1 (w1 : bit_word n1) 
-              n2 (w2 : bit_word n2),
-      s1 -[ w1 ]*-> s2 -> s2 -[ w2 ]-> s3 -> s1 -[ w1 ∘ w2 ]*-> s3
- 
- where "st1 -[ c ]*-> st2" := (PRIM_star st1 c st2).
-    
+  Definition PRIM_star X := fsa_run (PRIM_fsa X).
+     
 End PRIM_automaton.
 
 
