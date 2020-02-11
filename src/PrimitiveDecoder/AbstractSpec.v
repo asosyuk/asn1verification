8a02af89823a80e6b055a84e775fcda22b101227
Nika <npona@digamma.ai>
2020-02-11 19:26:39 +0200

    bool relational spec; tag and length specs structure

--- src/PrimitiveDecoder/AbstractSpec.v
+++ src/PrimitiveDecoder/AbstractSpec.v
@@ -1,7 +1,8 @@
 (* Abstract specification from the standard *)
 (* "It is implicit in the specification of these encoding rules 
        that they are also used for decoding" *)
-Require Import Core.Core.
+Require Import Core.Core. 
+Require Import Lists.List.
 Import ListNotations.
 
 Definition bit := bool.
@@ -11,7 +12,7 @@ Inductive bit_word : nat -> Type  :=
 
 Notation octet := (bit_word 8).
 
-(* end of contents depends on length *)
+Declare Scope word_scope.
 Notation "[[ ]]" := empty (format "[[ ]]") : word_scope.
 Notation "[[ x ]]" := (w _ x empty) : word_scope.
 Notation "[[ x ; y ; .. ; z ]]" := (w _ x (w _ y .. (w _ z empty) ..)) : word_scope.
@@ -24,20 +25,87 @@ Open Scope word_scope.
 
 Parameter nth_bit : nat -> octet -> bit.
 (* Last n bits of the octet *)
-Parameter high : nat -> octet -> list bit.
+Definition high n : forall m, bit_word m -> bit_word n.
+Admitted.
 (* First n bits of the octet *)
-Parameter low : nat -> octet -> list bit.
+Definition low n : forall m, bit_word m -> bit_word n.
+Admitted.
 
 (* tag encoding rules 8.1.2 *)
 
 Definition tag_class := bit_word 2.
+
+Definition universal_class := [[false; false]].
+Definition application_class := [[false; true]].
+(* etc *)
+
 Definition encoding_type := bit_word 1.
+
+Definition primitive_type := [[false]].
+Definition constructed_type := [[true]].
 
 Fixpoint copy n (b : bit) :=
   match n with
   | O => [[]]
   | S n => b:copy n b
   end.
+
+Definition all_zero : octet := copy 8 false.
+Definition all_one : octet := copy 8 true.
+
+(* convert integer into octets according to 8.1.2.4.1-2 *)
+Definition Z_to_bit_word n : Z -> bit_word n.
+Admitted.
+Parameter Z_to_octet : Z -> list octet.
+
+Definition big_tag z (tg: tag_class) (et: encoding_type) := 
+  (tg ∘ et ∘ (copy 5 true))::(Z_to_octet z).
+
+Definition small_tag (t : bit_word 5) (tg : tag_class) (et : encoding_type)  :=
+  [tg ∘ et ∘ t].
+
+Definition tag z := if z <=? 30 
+                    then small_tag (Z_to_bit_word 5 z)
+                    else big_tag z.
+
+(* Length encoding *)
+(* for primitive types the length encoding is in definite form *)
+Definition length z := if z <=? 127 
+                       then [([[false]] ∘ (Z_to_bit_word 7 z))]
+                       else let z := (Z_to_octet z) in
+                            let len := Zlength z in
+                            let l := Z_to_bit_word 7 len in
+                            ([[true]] ∘ l) :: z.
+
+Inductive bool_BER : bool -> list octet -> Prop:=
+  | False_BER (t l : Z) (tc : tag_class) :
+      bool_BER false (tag t tc primitive_type ++ (length l) ++ [all_zero])  
+       
+  | True_BER (t l : Z) (tc : tag_class) (o : octet) :
+      o <> all_zero ->
+      bool_BER true (tag t tc primitive_type ++ (length l) ++ [o]). 
+
+Inductive bool_DER : bool -> list octet -> Prop:=
+  | False_DER (t l : Z) (tc : tag_class) :
+      bool_DER false (tag t tc primitive_type ++ (length l) ++ [all_zero])  
+       
+  | True_DER (t l : Z) (tc : tag_class) :
+      bool_DER true (tag t tc primitive_type ++ (length l) ++ [all_one]). 
+
+Parameter ber_decode_bool : list octet -> option bool.
+Parameter der_encode_bool : bool-> list octet.
+
+Theorem roundtrip : forall ls b, ber_decode_bool (der_encode_bool b) = Some b.
+(* Yes *)
+ 
+Theorem encoder_correctness : BER_bool b (ber_encode_bool ls).
+
+Theorem decoder_correctness_left : forall ls b, ber_decode_bool ls = Some b -> BER_bool b ls.
+(* No, since ber_decode_bool (tag ∘ 02 ∘ ...) = Some b *)
+ 
+Theorem decoder_correctness_right : BER_bool b ls -> ber_decode_bool ls = Some b.
+(* Yes *)
+
 
 Fixpoint read_tag (ol: list octet) (ls : list bit) :=
   match ol with
@@ -51,14 +119,6 @@ Fixpoint read_tag (ol: list octet) (ls : list bit) :=
              end
   end.
 
-Parameter Z_to_octet : Z -> list octet.
-(* separate z into octets and add leading 1s and 0s, as per 8.1.2.4.2 b:
-b) bits 7 to 1 of the first subsequent octet, followed by bits 7 to 1 of the second subsequent octet, followed
-in turn by bits 7 to 1 of each further octet, up to and including the last subsequent octet in the identifier
-octets shall be the encoding of an unsigned binary integer equal to the tag number, with bit 7 of the first
-subsequent octet as the most significant bit; 
-
-*)
 
 Parameter make_tag : Z -> list octet -> list octet.
 
@@ -70,8 +130,6 @@ Fixpoint tag_number (z : Z) :=
        (copy 8 true) :: make_tag n z.
 
 
-Definition big_tag (tg: tag_class) (et: encoding_type) (z : Z) := tg ∘ et ∘ (copy 8 true).
-Definition small_tag (tg: tag_class) (et: encoding_type) (t : octet) := tg ∘ et ∘ t.
 Parameter bit_to_Z : list bit -> Z.
 
 Definition tag_to_Z (ol : list octet) := bit_to_Z (read_tag ol []).
