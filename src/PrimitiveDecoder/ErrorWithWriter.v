cb6dbf369a1fc1e7c19064673cbb8b07a07225f3
p3rsik <yroslav541@gmail.com>
2020-02-28 18:06:12 +0200

    Add custom exception monad errW [skip ci]

---
+++ src/PrimitiveDecoder/ErrorWithWriter.v
@@ -0,0 +1,47 @@
+From Coq Require Import String.
+Require Import ExtLib.Structures.Monad.
+Require Import ExtLib.Data.Monads.WriterMonad.
+Require Import ExtLib.Structures.MonadWriter.
+Require Import ExtLib.Structures.Monoid.
+Require Import ExtLib.Structures.MonadExc.
+
+Import MonadNotation.
+
+Open Scope monad.
+
+Section Error.
+
+Context {Wr : Type}.
+Context {MWr : Monoid Wr}.
+
+Definition errW A := Wr -> string + (Wr * A).
+
+Global Instance Monad_errW : Monad errW := {
+  ret := fun _ x => fun w => inr (w, x) ;
+  bind := fun _ _ m f => fun w => match m w with
+                            | inl v => inl v
+                            | inr (w', x) => f x w'
+                            end
+}.
+
+Global Instance Exception_errW : MonadExc string errW := {
+  raise := fun _ v => fun w => inl v ;
+  catch := fun _ c h => fun w => match c w with
+                           | inl v => h v w
+                           | inr x => inr x
+                           end
+}.
+
+Global Instance Writer_errW : MonadWriter MWr errW := {
+  tell := fun w => fun _ => inr (w, tt) ;
+  listen := fun _ m => fun w => match m w with 
+                          | inl v => inl v 
+                          | inr (w', x) => inr (w', (x, w'))
+                          end ;
+  pass := fun _ m => fun w => match m w with
+                        | inl v => inl v
+                        | inr (w', (x, f)) => inr (f w', x)
+                        end ;
+}.
+
+End Error.
