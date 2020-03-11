d302339303249cc9821b44c6d0c1a227a81cb98b
Nika <npona@digamma.ai>
2020-03-11 20:18:07 +0200

    fix VST bool spec

--- src/PrimitiveDecoder/BooleanVSTSpecDecoder.v
+++ src/PrimitiveDecoder/BooleanVSTSpecDecoder.v
@@ -10,8 +10,17 @@ Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 
 Open Scope Z.
 
+Section Boolean_ber_decode.
 
-Section Boolean_der_decode_primitive.
+Notation _asn_dec_rval_s_struct := (Tstruct _asn_dec_rval_s noattr).
+
+Definition asn_dec_rval_rep sh v code cons :=  
+ (field_at sh _asn_dec_rval_s_struct 
+           [StructField _code]
+           (Vint (Int.repr code)) v *
+  field_at sh _asn_dec_rval_s_struct 
+           [StructField _consumed] 
+           (Vint (Int.repr cons)) v)%logic. 
 
 Definition bool_ber_decode_spec : ident * funspec :=
   DECLARE _BOOLEAN_decode_ber
@@ -20,13 +29,13 @@ Definition bool_ber_decode_spec : ident * funspec :=
          (* Type Descriptor pointer *)
          sh_td : share, td_b : block, td_ofs : ptrofs, td : TYPE_descriptor,
          (* Value double pointer *)
-         sh_val' : share, val_b' : block, val_ofs' : ptrofs,
-         sh_val : share, val_b : block, val_ofs : ptrofs,
+         sh_val: share, bool_value : val, (* pointer can be null *)
          (* Buffer pointer *)
          sh_buf : share, buf_b : block, buf_ofs : ptrofs, buf : list byte,
+         (* pointer to the return struct dec_rval *)                        
+         sh_res : share, res_b : block, res_ofs : ptrofs,
          size : Z, tag_mode : Z
-    PRE  [         
-         (* added by clightgen - since returning structs is not supported *) 
+    PRE  [
          __res OF (tptr (Tstruct _asn_dec_rval_s noattr)),
          _opt_codec_ctx OF (tptr (Tstruct _asn_codec_ctx_s noattr)),
          _td OF (tptr (Tstruct _asn_TYPE_descriptor_s noattr)),
@@ -36,51 +45,54 @@ Definition bool_ber_decode_spec : ident * funspec :=
          _tag_mode OF tint]
     PROP ( readable_share sh_ctx; 
            readable_share sh_td; 
-           readable_share sh_val';
-           readable_share sh_val;
-           readable_share sh_buf )
+           readable_share sh_buf;
+           writable_share sh_res;
+           is_pointer_or_null bool_value
+            )
     LOCAL ( temp _opt_codec_ctx ctx;
             temp _td (Vptr td_b td_ofs);
-            temp _bool_value (Vptr val_b' val_ofs');
+            temp _bool_value bool_value;
             temp _buf_ptr (Vptr buf_b buf_ofs);
             temp _size (Vint (Int.repr size));
-            temp _tag_mode (Vint (Int.repr tag_mode)))
-    SEP (
-           data_at sh_ctx (tptr (Tstruct _asn_codec_ctx_s noattr))
-                   (Vptr ctx_b ctx_ofs) (ctx);
-           data_at sh_td (Tstruct _asn_TYPE_descriptor_s noattr)
+            temp _tag_mode (Vint (Int.repr tag_mode));
+            temp _st bool_value)
+    SEP (data_at sh_ctx (tptr (Tstruct _asn_codec_ctx_s noattr))
+                   (Vptr ctx_b ctx_ofs) ctx;
+         data_at sh_td (Tstruct _asn_TYPE_descriptor_s noattr)
                    (TYPE_descriptor_rep td) (Vptr td_b td_ofs);
-           (* val double pointer *)
-           data_at sh_val' (tptr (tptr tuchar)) (Vptr val_b val_ofs)
-                   (Vptr val_b' val_ofs');
-           data_at sh_val (tptr tuchar) (default_val (tptr tuchar)) (Vptr val_b val_ofs);
-           (* buf points to data ls *)
-           data_at sh_buf (tarray tschar (Zlength buf)) (map Vbyte buf) 
+         data_at sh_buf (tarray tschar (Zlength buf)) (map Vbyte buf) 
                    (Vptr buf_b buf_ofs))
     POST [tvoid]
       PROP()
       LOCAL ()
-      SEP( 
-           (* Unchanged by the execution : *)
+      SEP( (* Unchanged by the execution : *)
            data_at sh_ctx (tptr (Tstruct _asn_codec_ctx_s noattr))
-                   (Vptr ctx_b ctx_ofs) (ctx);
+                   (Vptr ctx_b ctx_ofs) ctx;
            data_at sh_td (Tstruct _asn_TYPE_descriptor_s noattr) 
                    (TYPE_descriptor_rep td) (Vptr td_b td_ofs);
            data_at sh_buf (tarray tschar (Zlength buf)) (map Vbyte buf) 
                    (Vptr buf_b buf_ofs);
-           data_at sh_val' (tptr (tptr tuchar)) (Vptr val_b val_ofs) 
-                   (Vptr val_b' val_ofs');
-           (* Changed after execution *)
-           let res := bool_decoder td buf in
+           (* Changes according to the exec spec *)
+           let RC_FAIL := asn_dec_rval_rep sh_res (Vptr res_b res_ofs) 2 0 in
+           if Val.eq bool_value Vnullptr 
+           then RC_FAIL
+           else let res := bool_decoder td buf in
            match res with
-           | Some r => data_at sh_val (tptr tuchar) (Vbyte r) (Vptr val_b val_ofs)
-           | None => data_at sh_val (tptr tuchar) (default_val (tptr tuchar)) (Vptr val_b val_ofs)
+           | Some (r, c) => asn_dec_rval_rep sh_res (Vptr res_b res_ofs) 0 c * 
+                            data_at sh_val (tptr tuchar) (Vbyte r) bool_value
+           | None => RC_FAIL *
+                     data_at sh_val (tptr tuchar)
+                             (default_val (tptr tuchar)) bool_value
            end).
+           
 
 Definition Gprog2 := ltac:(with_library prog [bool_ber_decode_spec]).
 
+
 Theorem bool_ber_decode : semax_body Vprog Gprog2 f_BOOLEAN_decode_ber
                                      bool_ber_decode_spec.
+
+Fail start_function.
 Admitted.
 
-End Boolean_der_decode_primitive.
+End Boolean_ber_decode.
