9588b4b60b42cc52455aec723234444d57c4ec54
p3rsik <yroslav541@gmail.com>
2020-04-02 19:07:07 +0300

    Fix Low_tag to test only 7nth bit

--- src/Primitive/AbstractSpec.v
+++ src/Primitive/AbstractSpec.v
@@ -10,7 +10,7 @@ Require Import StructTact.StructTactics.
 Inductive Tag : list byte -> Prop :=
 (* 8.1.2.2 *)
 (* tag number < 31 *)
-| Low_tag t : negb (Byte.eq t all_one) = true -> Tag [t]
+| Low_tag t : t @ 7 = false -> Tag [t]
 (* 8.1.2.4 *)
 (* tag number >= 31 *)
 | High_tag t ls : forall n,
