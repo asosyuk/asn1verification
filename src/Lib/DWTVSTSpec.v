3b70e9bde81e955ad6ca5bfa09c76d4d28c76afe
p3rsik <yroslav541@gmail.com>
2020-04-09 13:43:02 +0300

    Fix DWT vst spec; Fix SEP clause [skip ci]

--- src/Lib/DWTVSTSpec.v
+++ src/Lib/DWTVSTSpec.v
@@ -12,44 +12,42 @@ Section Der_write_tags.
 Definition der_write_tags_spec : ident * funspec :=
   DECLARE _der_write_tags
     WITH (* pointer to type descriptor structure *) 
-         sd_b : block, sd_ofs : ptrofs, sd : TYPE_descriptor, sh_sd : share,
-         struct_l : Z, tag_mode : Z, last_tag_form : Z, tag : Z,
+         td_p : val, td : TYPE_descriptor, sh_td : share,
+         struct_len: Z, tag_mode : Z, last_tag_form : Z, tag : Z,
          (* callback pointer *)
-         cb_b : block, cb_ofs : ptrofs, cb_val : val, sh_cb : share,
+         cb_p : val, cb_val : val, sh_cb : share,
          (* callback argument pointer *)
-         app_key_b : block, app_key_ofs : ptrofs,
-         app_key_val : val, sh_app_key : share
+         app_p : val, app_key_val : val, sh_app_key : share
     PRE [ _sd OF (tptr (Tstruct _asn_TYPE_descriptor_s noattr)),
           _struct_length OF tuint, _tag_mode OF tint,
           _last_tag_form OF tint, _tag OF tuint,
-          _cb OF cb_type, _app_key OF (tptr tvoid) ]
-    PROP ( readable_share sh_sd;
+          _cb OF tptr cb_type, _app_key OF tptr tvoid ]
+    PROP ( readable_share sh_td;
            readable_share sh_cb;
            readable_share sh_app_key )
-    LOCAL ( temp _sd (Vptr sd_b sd_ofs);
-            temp _struct_length (Vint (Int.repr struct_l));
+    LOCAL ( temp _sd td_p;
+            temp _struct_length (Vint (Int.repr struct_len));
             temp _tag_mode (Vint (Int.repr tag_mode));
             temp _last_tag_form (Vint (Int.repr last_tag_form));
             temp _tag (Vint (Int.repr tag));
-            temp _cb (Vptr cb_b cb_ofs);
-            temp _app_key (Vptr app_key_b app_key_ofs))
-    SEP ( data_at sh_sd (Tstruct _asn_TYPE_descriptor_s noattr) 
-                  (TYPE_descriptor_rep sd) (Vptr sd_b sd_ofs) ; 
-          data_at sh_app_key (tvoid) (tt) (Vptr app_key_b app_key_ofs) ;
-          data_at sh_cb (cb_type) (tt) (Vptr cb_b cb_ofs))
+            temp _cb cb_p;
+            temp _app_key app_p)
+    SEP ( data_at sh_td (Tstruct _asn_TYPE_descriptor_s noattr) 
+                  (TYPE_descriptor_rep td) td_p ; 
+          data_at sh_app_key (tvoid) (tt) app_p ;
+          data_at sh_cb (cb_type) (tt) cb_p)
     POST [tint]
       PROP ()
       LOCAL (temp ret_temp (Vint (Int.repr (
-                                      match evalErrW (der_write_tags sd) [] with
+                                      match evalErrW (der_write_tags td) [] with
                                       | Some w => encoded w
                                       | None => -1
                                       end))))
       SEP ( (* Unchanged by the execution : *)
-            data_at sh_sd (Tstruct _asn_TYPE_descriptor_s noattr) 
-                    (TYPE_descriptor_rep sd) (Vptr sd_b sd_ofs) ; 
-            data_at sh_app_key (tvoid) (tt) 
-                    (Vptr app_key_b app_key_ofs) ;
-            data_at sh_cb (cb_type) (tt) (Vptr cb_b cb_ofs)).
+            data_at sh_td (Tstruct _asn_TYPE_descriptor_s noattr) 
+                    (TYPE_descriptor_rep td) td_p ; 
+            data_at sh_app_key (tvoid) (tt) app_p ;
+            data_at sh_cb (cb_type) (tt) cb_p).
 
 Definition Gprog := ltac:(with_library prog [der_write_tags_spec]).
 
--- src/Primitive/BooleanVSTSpecEncoder.v
+++ src/Primitive/BooleanVSTSpecEncoder.v
@@ -1,14 +1,14 @@
 Require Import Core.Core Core.StructNormalizer VstLib 
         BooleanExecSpec ErrorWithWriter DWTVSTSpec.
 Require Import VST.floyd.proofauto Psatz.
-Require Import Clight.BOOLEAN Clight.der_encoder.
+Require Import Clight.BOOLEAN. (*Clight.der_encoder.*)
 
-Definition prog : Clight.program := 
+(*Definition prog : Clight.program := 
   mkprogram composites 
             (Clight.BOOLEAN.global_definitions ++ 
              Clight.der_encoder.global_definitions) 
             (Clight.BOOLEAN.public_idents ++ Clight.der_encoder.public_idents)
-            _main Logic.I.
+            _main Logic.I.*)
 
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
 Instance CompSpecs : compspecs. make_compspecs prog. Defined.
@@ -19,21 +19,20 @@ Section Boolean_der_encode_primitive.
 
 Definition bool_der_encode_spec : ident * funspec :=
   DECLARE _BOOLEAN_encode_der
-    WITH sptr_b : block, sptr_ofs : ptrofs, sptr_val : Z, sh_sptr : share,
+    WITH sptr_p : val, sptr_val : Z, sh_sptr : share,
          (* pointer to the DEF table for the type decoded *)
-         td_b : block, td_ofs : ptrofs, sh_td : share,
-         td : TYPE_descriptor,
-         tag_mode : Z, tag_class : Z,
+         td_p : val, sh_td : share, td : TYPE_descriptor,
+         tag_mode : Z, tag : Z,
          (* callback pointer *)
-         cb_b : block, cb_ofs : ptrofs, cb_val : val, sh_cb : share,
+         cb_p : val, cb_val : val, sh_cb : share,
          (* callback argument pointer *)
-         app_key_b : block, app_key_ofs : ptrofs,
-         app_key_val : val, sh_app_key : share
+         app_p : val, app_key_val : val, sh_app_key : share
     PRE  [         
           (* added by clightgen - since returning structs is not supported *) 
           __res OF (tptr enc_rval_s),
           _td OF (tptr type_descriptor_s),
-          _sptr OF (tptr tvoid), _tag_mode OF tint,
+          _sptr OF (tptr tvoid), 
+          _tag_mode OF tint,
           _tag OF tuint,
           _cb OF (tptr cb_type),
           _app_key OF (tptr tvoid)
@@ -42,29 +41,25 @@ Definition bool_der_encode_spec : ident * funspec :=
            writable_share sh_sptr; 
            readable_share sh_cb;
            readable_share sh_app_key )
-    LOCAL ( temp _td (Vptr td_b td_ofs);
-            temp _sptr (Vptr sptr_b sptr_ofs);
+    LOCAL ( temp _td td_p;
+            temp _sptr sptr_p;
             temp _tag_mode (Vint (Int.repr tag_mode));
-            temp _tag (Vint (Int.repr tag_class));
-            temp _cb (Vptr cb_b cb_ofs);
-            temp _app_key (Vptr app_key_b app_key_ofs))
+            temp _tag (Vint (Int.repr tag));
+            temp _cb cb_p;
+            temp _app_key app_p)
     SEP ((* td points to td with readable permission *)
-           data_at sh_td type_descriptor_s 
-                   (TYPE_descriptor_rep td) (Vptr td_b td_ofs) ; 
-           data_at sh_sptr (tvoid) (tt) (Vptr sptr_b sptr_ofs) ;
-           data_at sh_app_key (tvoid) (tt) 
-                   (Vptr app_key_b app_key_ofs) ;
-           data_at sh_cb (cb_type) (tt) (Vptr cb_b cb_ofs))
+           data_at sh_td type_descriptor_s (TYPE_descriptor_rep td) td_p ; 
+           data_at sh_sptr (tvoid) (tt) sptr_p ;
+           data_at sh_app_key (tvoid) (tt) app_p ;
+           data_at sh_cb (cb_type) (tt) cb_p)
     POST [tvoid]
       PROP()
       LOCAL ()
       SEP( (* Unchanged by the execution : *)
-           data_at sh_td type_descriptor_s (default_val type_descriptor_s) 
-                   (Vptr td_b td_ofs) ; 
-           data_at sh_sptr (tvoid) (tt) (Vptr sptr_b sptr_ofs) ;
-           data_at sh_app_key (tvoid) (tt) 
-                   (Vptr app_key_b app_key_ofs) ;
-           data_at sh_cb (cb_type) (tt) (Vptr cb_b cb_ofs)).
+           data_at sh_td type_descriptor_s (default_val type_descriptor_s) td_p ; 
+           data_at sh_sptr (tvoid) (tt) sptr_p ;
+           data_at sh_app_key (tvoid) (tt) app_p ;
+           data_at sh_cb (cb_type) (tt) cb_p).
 
 Definition Gprog := ltac:(with_library prog [der_write_tags_spec; 
                                                bool_der_encode_spec]).
@@ -75,6 +70,10 @@ Theorem bool_der_encode : semax_body Vprog Gprog
                                      bool_der_encode_spec.
 Proof.
   start_function.
+  (*unfold fold_right; apply semax_frame;[lazy;auto|].*)
+  forward.
+  forward_call (td_p, td, sh_td, 1, tag_mode, 0, tag, cb_p, 
+                cb_val, sh_cb, app_p, app_key_val, sh_app_key).
 Admitted.
 
 End Boolean_der_encode_primitive.
