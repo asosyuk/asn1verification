01a226a81039befadb2d2074c2686b49e93a58c4
Nika <npona@digamma.ai>
2020-04-23 18:53:11 +0300

    overrun callback spec

--- src/Lib/VstCallback.v
+++ src/Lib/VstCallback.v
@@ -9,36 +9,41 @@ Open Scope Z.
 
 Section Encoder_callback.
 
-Definition enc_key := Tstruct _overrun_encoder_key noattr.
+Definition enc_key_s := Tstruct _overrun_encoder_key noattr.
 
-Definition constr_enc_key buf size comp_size : reptype enc_key := 
-  (buf, (Vint (Int.repr size), Vint (Int.repr comp_size))).
+Definition mk_enc_key buf size comp_size : reptype enc_key_s := 
+  (buf, (Vint (Int.repr size), Vint (Int.repr comp_size))). 
 
-Definition callback : funspec :=
-    WITH data : list int, data_p : val, size : Z, app_key_p : val
-    PRE[tptr tvoid, tuint, tptr tvoid]
-      PROP()
-      PARAMS(data_p; Vint (Int.repr size); app_key_p)
-      GLOBALS()
-      SEP((*data_at Tsh (tarray tuchar (Zlength data)) (map Vint data) data_p;
-          data_at_ Tsh tvoid app_key_p ;
-          data_at Tsh enc_key (constr_enc_key) key_p *) )
-    POST[tint]
-      PROP()
-      LOCAL(temp ret_temp (Vint (Int.repr 0)))
-      SEP((*data_at Tsh (tarray tuchar (Zlength data)) (map Vint data) data_p;
-          data_at_ Tsh tvoid app_key_p *)).
-
-Definition cb := 9999999999%positive. 
-Definition callback_spec := DECLARE cb callback.
+Definition callback  : funspec :=
+    WITH b : bool, data : list int, data_p : val, size : Z,
+         app_key : val, app_key_p : val, 
+         buf_b : block, buf_ofs : ptrofs,
+         buf_size : Z, computed_size : Z
+    PRE [tptr tvoid, tuint, tptr tvoid]
+      PROP ()
+      PARAMS (data_p; Vint (Int.repr size); app_key_p)
+      GLOBALS ()
+      SEP (data_at Tsh (tarray tuchar (Zlength data)) (map Vint data) data_p;
+           data_at Tsh enc_key_s 
+                   (mk_enc_key (Vptr buf_b buf_ofs) buf_size computed_size) 
+                   app_key_p;
+           data_at_ Tsh tvoid (Vptr buf_b buf_ofs))
+    POST [tint]
+      PROP ()
+      LOCAL (temp ret_temp Vzero)
+      SEP (data_at Tsh (tarray tuchar (Zlength data)) (map Vint data) data_p;
+           data_at Tsh (tarray tuchar (Zlength data)) (map Vint data) 
+                   (Vptr buf_b (Ptrofs.add buf_ofs (Ptrofs.repr computed_size))); 
+           data_at Tsh enc_key_s 
+                  (mk_enc_key (Vptr buf_b buf_ofs) buf_size size) app_key_p).
 
 Definition callback_overrun_spec : ident * funspec :=
   DECLARE _overrun_encoder_cb callback.
 
-Definition Gprog := ltac:(with_library prog [callback_spec]).
+Definition Gprog := ltac:(with_library prog [callback_overrun_spec]).
 
 Theorem bool_der_encode : semax_body Vprog Gprog f_overrun_encoder_cb 
-                                     callback_spec.
+                                     callback_overrun_spec.
 Admitted.
 
-End Encoder_callback.
+End Encoder_callback. 
