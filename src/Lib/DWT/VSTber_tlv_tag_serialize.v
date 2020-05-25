e2528a3f8ba8e95d40114064cbf0cbaa0d7ca7d0
Nika <npona@digamma.ai>
2020-05-25 20:53:36 +0300

    sep proof for case tag > 30 [skip ci]

--- src/Lib/DWT/VSTber_tlv_tag_serialize.v
+++ src/Lib/DWT/VSTber_tlv_tag_serialize.v
@@ -124,6 +124,58 @@ Definition ber_tlv_tag_serialize_spec' : ident * funspec :=
 
 Definition Gprog' := ltac:(with_library prog [ber_tlv_tag_serialize_spec']).
 
+Arguments valid_pointer p : simpl never.
+
+Open Scope Z.
+
+Proposition split_non_empty_list (cs : compspecs) i ls' ls sh b ofs:
+      ls = i::ls'  -> Ptrofs.unsigned ofs + Zlength ls < Ptrofs.modulus -> 
+      data_at sh (tarray tuchar (Zlength ls)) (map Vbyte ls) (Vptr b ofs) =
+      (data_at sh tuchar (Vbyte i) (Vptr b ofs) *
+      data_at sh (tarray tuchar (Zlength ls')) (map Vbyte ls')
+              (Vptr b (Ptrofs.add ofs Ptrofs.one)))%logic.
+Proof.
+  intros LEN MOD.
+  rewrite LEN.
+  replace (i::ls') with ([i] ++ ls') by reflexivity.
+  rewrite map_app. 
+  rewrite split2_data_at_Tarray_app with (mid := 1).
+  assert (map Vbyte [i] = [Vbyte i]) as T by reflexivity.
+  pose proof data_at_singleton_array_eq sh tuchar (Vbyte i) 
+       (map Vbyte [i]) (Vptr b ofs) T as T1; rewrite T1; clear T T1.
+
+  assert (Vptr b (Ptrofs.add ofs Ptrofs.one) =
+          field_address0 (tarray tuchar (Zlength (app [i] ls'))) [ArraySubsc 1]
+                         (Vptr b ofs))
+    as J.
+  { 
+    rewrite field_address0_offset.
+    reflexivity.
+    econstructor.
+    easy.
+    repeat split.
+    simpl; autorewrite with norm.
+    rewrite <- LEN.
+    eassumption.
+    constructor.
+    intros.
+    repeat econstructor.
+    simpl; autorewrite with norm.
+    reflexivity.
+    all: try nia || auto with zarith.
+    autorewrite with sublist.
+    simpl.
+    pose proof (Zlength_nonneg ls').
+    nia.
+  }
+  rewrite J.
+  replace (Zlength (app [i] ls') - 1) with (Zlength ls').
+  reflexivity.
+  all: try autorewrite with sublist; auto.
+Qed.
+
+Open Scope IntScope.
+
 Theorem ber_tlv_tag_serialize_correct' : 
   semax_body Vprog Gprog' (normalize_function f_ber_tlv_tag_serialize composites)
              ber_tlv_tag_serialize_spec'.
@@ -171,10 +223,48 @@ Proof.
         simpl;
         erewrite Zlength_list_repeat;
         try nia; auto.
-  - forward_if True.
-    + forward.
-      forward.
-            admit.
+  - (* 30 < tag *)
+   forward_if (
+       PROP()
+       LOCAL(if eq_dec size 0 
+             then temp _buf__1 bufp
+             else temp _buf__1 (offset_val 1 bufp)  ;
+             if eq_dec size 0 
+             then temp _size (Vint size)
+             else temp _size (Vint (size - 1)))
+       SEP(if eq_dec size 0 
+           then data_at_ Tsh (tarray tuchar buf_size) bufp 
+           else (data_at Tsh (tarray tuchar buf_size)
+    (upd_Znth 0 (default_val (tarray tuchar buf_size))
+       (Vint
+          (Int.zero_ext 8
+             (Int.or (Int.shl 
+                        ((tag & Int.repr 3)) (Int.repr 6)) 
+                     (Int.repr 31))))) bufp))).
+    + unfold tarray. 
+      erewrite split2_data_at__Tarray_tuchar with (n1 := 1%Z).
+      replace (data_at_ Tsh (Tarray tuchar 1 noattr) bufp) 
+        with (data_at_ Tsh tuchar bufp).
+      Intros.
+      repeat forward.
+      repeat rewrite_if_b.
+      erewrite upd_Znth_unfold.
+      replace (len (default_val (tarray tuchar buf_size))) with buf_size.
+                entailer!.
+      unfold tarray.
+      erewrite split2_data_at_Tarray_tuchar with (n1 := 1%Z).
+      autorewrite with sublist.
+      entailer!.
+      admit.
+      all: try nia;
+        unfold default_val;
+        simpl;
+        try erewrite Zlength_list_repeat;
+        try nia; auto.
+      admit.
+      admit.
+      admit.
+      admit.
     + forward. entailer!.
     + repeat forward.
       (* loop *)
