8998713aea18a87d10cbae48c12477c6d83d87cd
Nika <npona@digamma.ai>
2020-09-11 20:44:05 +0300

    ASN__STACK_OVERFLOW_CHECK VST spec and part of the proof [skip ci]

---
+++ src/Lib/BCT/VST/ASN__STACK_OVERFLOW_CHECK.v
@@ -0,0 +1,78 @@
+Require Import Core.Core Core.StructNormalizer
+        VstLib ErrorWithWriter BCT.Exec.
+Require Import VST.floyd.proofauto.
+Require Import Clight.ber_decoder.
+
+Definition Vprog : varspecs. mk_varspecs prog. Defined.
+Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+
+Eval cbn in reptype (Tstruct _asn_codec_ctx_s noattr).
+
+Definition ASN__STACK_OVERFLOW_CHECK used_stack max_stack_size := 
+  if eq_dec max_stack_size 0 
+  then 0
+  else if used_stack <? max_stack_size 
+       then -1
+       else 0.
+
+Definition ASN__STACK_OVERFLOW_CHECK_spec : ident * funspec :=
+  DECLARE _ASN__STACK_OVERFLOW_CHECK
+    WITH ctx_p : val, max_stack_size : Z
+    PRE [tptr (Tstruct _asn_codec_ctx_s noattr)]
+      PROP ()
+      PARAMS (ctx_p)
+      GLOBALS ()
+      SEP (data_at Tsh (Tstruct _asn_codec_ctx_s noattr)
+                   (Vint (Int.repr max_stack_size)) ctx_p)
+    POST [tint]
+      PROP ()
+      LOCAL (temp ret_temp 
+               (Vint
+                  (Int.repr 
+                     (if eq_dec ctx_p nullval 
+                      then 0
+                      else let usedstack := 0 in (* placeholder *)
+                           ASN__STACK_OVERFLOW_CHECK usedstack max_stack_size))))
+      SEP (data_at Tsh (Tstruct _asn_codec_ctx_s noattr) 
+                   (Vint (Int.repr max_stack_size)) ctx_p).
+
+Definition Gprog := ltac:(with_library prog [ASN__STACK_OVERFLOW_CHECK_spec]).
+
+Theorem bool_der_encode :
+  semax_body Vprog Gprog 
+             (normalize_function f_ASN__STACK_OVERFLOW_CHECK composites) 
+             ASN__STACK_OVERFLOW_CHECK_spec.
+Proof.
+  start_function.
+  repeat forward.
+  forward_if (temp _t'1 (Vint (Int.repr (if eq_dec ctx_p nullval  
+                                         then 0 
+                                         else (if eq_dec max_stack_size 0 
+                                               then 0 
+                                               else 1))))). 
+  - repeat forward.
+    entailer!.
+    unfold isptr in H.
+    destruct ctx_p; try contradiction.
+    unfold nullval.
+    break_if.
+    generalize e. break_if; try congruence.
+    destruct (Int.eq (Int.repr max_stack_size) Int.zero) eqn : N.
+    eapply int_eq_e in N.
+    break_if; auto; try rep_omega.
+    admit.
+    eapply int_eq_false_e in N.
+    break_if; auto.
+    erewrite e in *.
+    contradiction.
+  - forward.
+    entailer!.
+  - forward_if True.
+    admit.
+Admitted.
+
+    
+
+  
+
+
--- src/Lib/BCT/Vst.v
+++ src/Lib/BCT/Vst.v
@@ -10,6 +10,44 @@ Open Scope Z.
 
 Section Ber_check_tags.
 
+(* ts td ctx tag_mode size step *)
+
+(* typedef struct asn_struct_ctx_s {
+	short phase;		/* Decoding phase */
+	short step;		/* Elementary step of a phase */
+	int context;		/* Other context information */
+	void *ptr;		/* Decoder-specific stuff (stack elements) */
+	ber_tlv_len_t left;	/* Number of bytes left, -1 for indefinite */
+} asn_struct_ctx_t;
+*)
+
+Record asn_struct_ctx_type_abstract := 
+  {  phase : Z;		
+     step : Z;		
+     context : Z;		
+     left : Z }. 
+
+Definition asn_struct_ctx_composites :=
+  ((_phase, tshort) :: (_step, tshort) :: (_context, tint) ::
+    (_ptr, (tptr tvoid)) :: (_left, tint) :: nil).
+
+Fixpoint mk_struct_repr (ls : list (ident * type))  :=
+  match ls with
+    | [] => val
+    | [h] => let (_, t) := h in reptype t
+    | h :: tl => let (_, t) := h in
+                (reptype t * mk_struct_repr tl)%type
+  end.  
+
+Definition asn_struct_ctx_type := mk_struct_repr asn_struct_ctx_composites.
+
+(*  
+Definition ctx_rep c : reptype (Tstruct _asn_struct_ctx_s noattr) :=
+  (Vint (Int.repr (phase c)), Vint (Int.repr (step c)),
+   Vint (Int.repr (context c)), Vzero, Vint (Int.repr (left c))). 
+
+Eval cbn in reptype (Tstruct _asn_struct_ctx_s noattr). *)
+
 Definition ber_check_tags_spec : ident * funspec :=
   DECLARE _ber_check_tags
     WITH (* Codec context pointer *) 
@@ -19,30 +57,33 @@ Definition ber_check_tags_spec : ident * funspec :=
          (* Struct context pointer *) 
          ctx_s_p : val, ctx_s : val,
          (* Buffer pointer *)
-         buf_p : val, buf : list byte,
+         buf_p : val, buf : list Z,
          (* pointer to the return struct dec_rval *)                        
          res_p : val,
          size : Z, tag_mode : Z, last_tag_from : Z,
-         ll_p : val, opt_tlv_form_p : val, opt_tlv_form : Z
+         ll_p : val, opt_tlv_form_p : val, opt_tlv_form : Z,
+         step : Z, c : asn_struct_ctx_type, ctx_Z : asn_codec_ctx
     PRE [tptr asn_dec_rval_s, tptr (Tstruct _asn_codec_ctx_s noattr),
          tptr (Tstruct _asn_TYPE_descriptor_s noattr),
          tptr (Tstruct _asn_struct_ctx_s noattr), 
          tptr tvoid, tuint, tint, tint, tptr tint, tptr tint]
-      PROP ()
+      PROP (let (x, _) := let (_, y) := c in y in x
+            = Vint (Int.repr (if eq_dec ctx_s_p nullval then 0 else step)))
       PARAMS (res_p; ctx_p; td_p; ctx_s_p; buf_p; Vint (Int.repr size);
               Vint (Int.repr tag_mode); Vint (Int.repr last_tag_from);
               ll_p; opt_tlv_form_p)
       GLOBALS ()
       SEP (data_at_ Tsh asn_dec_rval_s res_p;
-           data_at_ Tsh tint ll_p)
+           data_at_ Tsh tint ll_p;
+           data_at Tsh (Tstruct _asn_struct_ctx_s noattr) c ctx_s_p)
     POST [tvoid]
       PROP ()
       LOCAL ()
-      SEP (match ber_check_tags td buf with
+      SEP (match ber_check_tags buf td ctx_Z tag_mode size step with
            | Some v => 
              data_at Tsh asn_dec_rval_s 
-                     (mk_dec_rval 0 (tag_consumed v)) res_p *
-             data_at Tsh tint (Vint (Int.repr (tag_length v))) ll_p
+                     (mk_dec_rval 0 v) res_p *
+             data_at Tsh tint (Vint (Int.repr v)) ll_p
            | None => 
              data_at Tsh asn_dec_rval_s (mk_dec_rval 2 0) res_p *
              data_at_ Tsh tint ll_p
@@ -51,6 +92,27 @@ Definition ber_check_tags_spec : ident * funspec :=
 Definition Gprog := ltac:(with_library prog [ber_check_tags_spec]).
 
 Theorem bool_der_encode : semax_body Vprog Gprog (normalize_function f_ber_check_tags composites) ber_check_tags_spec.
+Proof.
+  start_function.
+  repeat forward.
+  forward_if (temp _t'1 
+                   (Vint (Int.repr (if eq_dec ctx_s_p nullval 
+                                    then 0 
+                                    else step)))).
+  forward.    
+  entailer!.
+  repeat break_let.
+  subst.
+  inversion Heqp.
+  cbn.
+  (* add short bounds *)
+  admit.
+  forward.
+  entailer!.
+  admit.
+  forward.
+  entailer!.
+  forward.
 Admitted.
 
 End Ber_check_tags.
